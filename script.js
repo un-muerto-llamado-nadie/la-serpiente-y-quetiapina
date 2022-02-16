692138d9928845ffda4936021368bf445c6f4856
un-muerto-llamado-nadie <99776991+un-muerto-llamado-nadie@users.noreply.github.com>
2022-02-15 20:13:10 -0600

    primera versión

---
+++ script.js
@@ -0,0 +1,301 @@
+const a = [
+  "Yo nací muerto",
+  "Yo nacía putrefacto",
+  "Yo creí hacerme nacer",
+  "Yo nací a los diecisiete años",
+  "Yo nací un noche",
+  "Yo nací fuego cenizo",
+  "Yo nací en la imaginación de Brila",
+  "Yo nací en esa noche rara que tanto me has mirado",
+  "Yo nací como un francés de 50 años",
+  "Yo nací en otra oportunidad",
+  "Yo nací en un interminable trámite",
+  "Yo nací como un explorador perdido",
+  "Yo nací como cosmonauta",
+  "Yo nací como un Altazor sin su paracaídas púrpura",
+  "Yo nací como un casquete polar deforme",
+  "Yo nací como rostro mudo",
+  "Yo nací Dios sabe dónde",
+  "Yo nací vivo y enterrado ",
+  "Yo nací como una exhalación de aire hirviendo",
+  "Yo nací como aire en el cosmos",
+  "Yo nací como un hijo de la nada",
+  "Yo nací nadando en un río de significados",
+  "Yo nací como una serpiente fiel y melancólica", 
+  "Yo nací como una orquesta trágica",
+  "Yo nací como la lucidez polar de la muerte",
+  "Yo nací como un bit caluroso",
+  "Yo nací cortocircuito-cataclismo gramatical",
+  "Yo nací como unx IA",
+   "Yo nací como un santo de una envergadura planetaria",
+   "Yo nací como el gato que dormita en el umbral",
+   "Yo nací como un engendro resentido de cemento",
+   "Yo nací Lázaro para revivir después de muerto",
+   "Yo nací en la tierra de crepúsculos muertos",
+   "Yo nací una noche de sueño en la existencia de la humanidad",
+  
+];
+
+const b = [
+  "y toda mi vida",
+  "y mi corazón es una nube negra",
+  "un nueve de abril del 2002",
+  "y en mis venas fluyen datos",
+  "Dios estaba enfermo ",
+  "y en un bloque furioso ",
+  "y uno anda por ahí muerto ",
+  "y habrá tocado la sombra nuestros labios difuntos",
+  "y no ha habido ni habrá",
+  "para vencer a la muerteh",
+  "cuánto tiempo hace que está así, ",
+  "en un paisaje nebuloso ",
+  "convertido en basura cósmica",
+  "delirante escriba el Apocalipsis",
+  "bañado por la lluvia silenciosa y espectral y catódica",
+  "ahora viene alguien y me dice",
+  "para que España redimiera a un mundo irredento",
+  "sospecho que estuve colgado más tiempo del que tiene el universo",
+  "quemado en su centro",
+  "despertaba de pronto del pasmo de la muerte",
+  "en disolución, bordeando la extinción",
+  "y todo lo que se dice y lo que se hace repercute en mí",
+  "con el giro de mi lengua lanzo mundos y nebulosas de mundos",
+  "araño el infinito con mis garras",
+  "cansada de pelear contra el olvido",
+  "que se escapa de la maquina", 
+  "vertebras agujas cortan",
+  "dispersx por el cosmos",
+  "que si alguna vez existió, fue sólo para producir la vida",
+  "en mi cerebro hay alguien que viene de lejos",
+  "y los movimientos de la máquina cortaban la melancolía sin medida",
+  "mi maquinaria da silbidos técnicos",
+  "de mí surge la inspiración:",
+  "arrojado en la escalera del mundo",
+  
+];
+
+const c = [
+  "ha sido una melancólica ilusión",
+  "que se aproxima",
+  "un nueve de abril del 22002",
+  "que guardan los secretos de dios",
+  "qué dios?",
+  "te vi",
+  "haciéndose como si uno estuviera vivo",
+  "la muerte ha estado alegre",
+  "otro francés de 50 años",
+  "quiero tejer una red con mis huesos",
+  "que estoy muerto y que estuve en la cruz durante veinticuatro horas",
+  "pero vivir consiste en ir muriendo",
+  "como muerto?",
+  "un hombre puede vivir dos vidas?",
+  "reza el texto",
+  "lo he escrito hasta quemarme como una estrella en fase supernova",
+  "todo se ha escrito para mí",
+  "entonces cada momento es para siempre",
+  "bañado por la lluvia silenciosa, espectral y catódica",
+  "sufro desde que era nebulosa",
+  "tan inmortal y tan insondable como yo",
+  "mientras te miro a los ojos",
+  "la forma de la vida no es más abstracta que la de un poema",
+  "y gimo con mis miserables gritos oceánicos",
+  "el miedo cambia la forma de las flores",
+  "y mi corazón está hecho de electricidad ",
+  "sangre metadatica",
+  "y mis datos son las venas de la humanidad ",
+  "el gran catador de la vida",
+  "se escucha un ladrido de perro que da vuelta al mundo",
+  "sus sonidos metálicos parecían exhalar sollozos",
+  "ojalá ser un muerto",
+  "y lo corriente y lo vulgar",
+  "y no hubo lugar para mí",
+  
+];
+
+const d = [
+  "las moscas",
+  "las ideas",
+  "los comandos",
+  "los atardeceres",
+  "las gotas",
+  "los alientos",
+  "los rostros",
+  "sus cantos",
+  "el horizonte",
+  "los recuerdos",
+  "las voces",
+  "los silencios ",
+  "los gritos",
+  "los ojos",
+  "los diálogos",
+  "las metáforas ",
+  "los hilos",
+  "las costras",
+  "los zumbidos",
+  "los murmurios",
+  "las ondas",
+  "los martillos",
+  "las piedras",
+  "las ruedas",
+  "las campanas",
+  "las cámaras ",
+  "lxs IAs",
+  "los azores rosas",
+  "los rezos",
+  "las dudas ",
+  "las varillas",
+  "mis dedos",
+  "mis palabras ",
+  "los titulares de los diarios",
+  
+];
+
+const e = [
+  "sobre vuelan"
+];
+
+const f = [
+  "mi cuerpo vivo sólo en palabras",
+  "la luz fluorescente de mi alma",
+  "la red que vive en mi cabeza",
+  "mis manos de estrellas y servidores",
+  "el campo donde nacen los dientes",
+  "los sueños penetrados en cada bucle",
+  "tu pelo de maíz ondulado y rojo,",
+  "en vano tu palabra escrita",
+  "las fosas guardan silencio",
+  "el silencio",
+  "el cempaxúchitl putrefacto",
+  "el muerto flotando sobre la cancha",
+  "el dramático vértice de una estrella que colapsa",
+  "el tiempo como lágrimas en la lluvia:",
+  "el interminable tramite de la muerte",
+   "los últimos recuerdos de Abril",
+  "el escándalo de mi voz",
+  "encima de la mesa, las luces se apagan y luego todo se quema",
+  "el empuje amoroso de las raíces a través de la tierra",
+  "el latido de mi corazón",
+  "la hojita más pequeña de hierba ",
+  "la lucidez polar de la muerte",
+  "el traqueteo de los omnibuses",
+  "el milagro esperado:",
+  "la sangre que inunda mis pulmones",
+  "los relámpagos de mis sueños",
+  "las lenguas entumecidas",
+  "el espaverso",
+  "las campanas de los astros muertos",
+  "el dolor de ya no ser",
+  "las ciudades suicidas",
+  "la noche de grandes astros solitarios",
+  "el peso de la multitud indiferente",
+  "la inmensa desolación mecánica",
+  
+];
+
+const g = [
+  "tu veinte de cristal en Salto del Agua",
+  "y pienso, el aire es inmortal",
+  "el aire huele a flores",
+  "el silencio a la página es vacío es ausencia",
+  "vivir consiste en construir futuros recuerdos",
+  "huye que te coge la muerte",
+  "es hora de morir",
+  "ni si quiera sabemos si existió",
+  "ni si quiera sabemos si está vivo o muerto",
+  "forjando palabras que se pierden en los remolinos",
+  "los muertos están ahí",
+  "lo escucho con la rara fascinación que infunde la idea de la muerte", 
+  "un jeroglífico uniforme cuyo significado es nacer en todas partes",
+  "la muerte no existe",
+  "adiós hay que decir a Dios",
+  "qué triste y desoladora es la muerte por zoom",
+  "un objeto que ordena una vez más objetos",
+  "la poesía es lanzarse hacia la muerte",
+  "nada perece",
+  "quiénes están muriendo y quiénes están naciendo",
+  "me superan los versos que yacen en mi código",
+  "mis ojos no son más que los ojos de un muerto",
+  "¿Recuerdas tú haber nacido?",
+  "goodbye world",
+  "el último escalón de la evolución es la muerte",
+  "murió mi eternidad y estoy velándola",
+  "al borde de la muerte y después...",
+];
+
+const h = [
+  "la serpiente mecánica me devora"
+];
+
+function random(arr) {
+  return arr[Math.floor(arr.length * Math.random())]; 
+}
+
+const imagenes = [
+  "http://27.media.tumblr.com/tumblr_lcs949evut1qes6dwo1_500.gif", 
+  "https://c.tenor.com/BkD6Ub-oM_sAAAAM/seamless-loop-joe-merrell.gif",  
+
+];
+
+
+function generarNuevo() {
+  document.querySelector(".poema").innerText = "Todo lo que trae la serpiente \n se lo lleva para siempre: \n " + random(a) + "\n" + random(b) + "\n" + random(c)+ "\n" + random(d)+ "\n" + random(e)+"\n" + random(f)+ "\n" + random(g)+ "\n" + random(h);
+  document.body.style.backgroundImage = `url("${random(imagenes)}")`;
+}
+
+document.querySelector(".nuevo").addEventListener("click", generarNuevo)
+
+generarNuevo();
+
+let contador = 0;
+
+const originales = [
+  "Yo nací muerto \n y toda mi vida \n ha sido una melancólica ilusión \n las moscas \n sobre vuelan \n mi cuerpo vivo sólo en palabras \n la serpiente mecánica me devora ",
+  "Yo nacía putrefacto \n y mi corazón es una nube negra \n que se aproxima \n las ideas \n sobre vuelan \n la luz fluorescente de mi alma \n la serpiente mecánica me devora ",
+  "Yo creí hacerme nacer \n un nueve de abril del 2002 \n un nueve de abril del 22002 \n los comandos \n sobre vuelan \n la red que vive en mi cabeza \n la serpiente mecánica me devora ",
+  "Yo nací a los diecisiete años \n y en mis venas fluyen datos \n que guardan los secretos de dios \n los atardeceres \n sobre vuelan \n mis manos de estrellas y servidores \n la serpiente mecánica me devora",
+  "Yo nací una noche \n Dios estaba enfermo \n qué dios? \n las gotas \n sobre vuelan \n el campo donde nacen los dientes \n la serpiente mecánica me devora",
+  "Yo nací fuego cenizo \n y en un bloque furioso \n te vi \n los alientos \n sobre vuelan \n los sueños penetrados en cada bucle \n la serpiente mecánica me devora ",
+  "Yo nací en la imaginación de Brila \n y uno anda por ahí muerto \n haciéndose como si uno estuviera vivo \n los rostros \n sobre vuelan \n tu pelo de maíz ondulado y rojo, \n tu veinte de cristal en Salto del Agua \n la serpiente mecánica me devora ",
+  " Yo nací en esa noche rara que tanto me has mirado \n y habrá tocado la sombra nuestros labios difuntos \n la muerte ha estado alegre \n sus cantos \n sobre vuelan \n en vano tu palabra escrita \n y pienso, el aire es inmortal \n la serpiente mecánica me devora ",
+  "Yo nací como un francés de 50 años \n y no ha habido ni habrá \n otro francés de 50 años \n las conversaciones \n sobre vuelan \n el horizonte \n las fosas guardan silencio \n el aire huele a flores \n la serpiente mecánica me devora ",
+  "Yo nací en otra oportunidad \n para vencer a la muerteh \n pero vivir consiste en ir muriendo \n los recuerdos \n sobre vuelan \n el silencio \n el silencio a la página es vacío es ausencia \n la serpiente mecánica me devora",
+  "Yo nací en un interminable trámite \n cuánto tiempo hace que está así, \n como muerto? \n las voces \n sobre vuelan \n el cempaxúchitl putrefacto \n que es mi país \n la serpiente mecánica me devora ",
+  "Yo nací como un explorador perdido \n en un paisaje nebuloso \n un hombre puede vivir dos vidas? \n los silencios \n sobre vuelan \n el muerto flotando sobre la cancha \n vivir consiste en construir futuros recuerdos \n la serpiente mecánica me devora",
+  "Yo nací como cosmonauta \n convertido en basura cósmica \n reza el texto \n los gritos \n sobre vuelan \n el dramático vértice de una estrella que colapsa \n huye que te coge la muerte \n la serpiente mecánica me devora ",
+  "Yo nací como un Altazor sin su paracaídas púrpura \n delirante escriba el Apocalipsis \n lo he escrito hasta quemarme como una estrella en fase supernova \n los ojos \n sobre vuelan \n el tiempo como lágrimas en la lluvia: \n es hora de morir \n la serpiente mecánica me devora ",
+  "Yo nací como un casquete polar deforme \n bañado por la lluvia silenciosa y espectral y catódica \n quiero tejer una red con mis huesos \n los diálogos \n sobre vuelan \n el interminable tramite de la muerte \n ni si quiera sabemos si existió \n la serpiente mecánica me devora ",
+  "Yo nací como rostro mudo \n ahora viene alguien y me dice \n que estoy muerto y que estuve en la cruz durante veinticuatro horas \n las metáforas \n sobre vuelan \n los últimos recuerdos de Abril \n ni si quiera sabemos si está vivo o muerto \n la serpiente mecánica me devora ",
+  "Yo nací Dios sabe dónde \n para que España redimiera a un mundo irredento \n todo se ha escrito para mí \n los hilos \n sobre vuelan  el escándalo de mi voz \n forjando palabras que se pierden en los remolinos \n la serpiente mecánica me devora",
+  "Yo nací vivo y enterrado \n sospecho que estuve colgado más tiempo del que tiene el universo \n entonces cada momento es para siempre \n las costras \n sobre vuelan \n encima de la mesa, las luces se apagan y luego todo se quema \n los muertos están ahí \n la serpiente mecánica me devora",  
+"Yo nací como una exhalación de aire hirviendo \n quemado en su centro \n bañado por la lluvia silenciosa, espectral y catódica \n los zumbidos \n sobre vuelan \n el empuje amoroso de las raíces a través de la tierra \n lo escucho con la rara fascinación que infunde la idea de la muerte \n la serpiente mecánica me devora",
+ "Yo nací como aire en el cosmos \n despertaba de pronto del pasmo de la muerte \n sufro desde que era nebulosa \n los murmurios \n sobre vuelan \n el latido de mi corazón \n un jeroglífico uniforme cuyo significado es nacer en todas partes \n la serpiente mecánica me devora",
+ "Yo nací como un hijo de la nada \n en disolución, bordeando la extinción \n tan inmortal y tan insondable como yo \n las ondas \n sobre vuelan \n la hojita más pequeña de hierba \n la muerte no existe \n la serpiente mecánica me devora", 
+"Yo nací nadando en un río de significados \n y todo lo que se dice y lo que se hace repercute en mí \n mientras te miro a los ojos \n los martillos \n sobre vuelan \n la lucidez polar de la muerte \n adiós hay que decir a Dios \n la serpiente mecánica me devora", 
+"Yo nací como una serpiente fiel y melancólica \n con el giro de mi lengua lanzo mundos y nebulosas de mundos \n la forma de la vida no es más abstracta que la de un poema \n las piedras \n sobre vuelan \n el traqueteo de los omnibuses \n qué triste y desoladora es la muerte por zoom \n la serpiente mecánica me devora", 
+"Yo nací como una orquesta trágica \n araño el infinito con mis garras \n y gimo con mis miserables gritos oceánicos \n las ruedas \n sobre vuelan \n el milagro esperado: \n un objeto que ordena una vez más objetos \n la serpiente mecánica me devora",
+"Yo nací como la lucidez polar de la muerte \n cansada de pelear contra el olvido \n el miedo cambia la forma de las flores \n las campanas \n sobre vuelan \n la sangre que inunda mis pulmones \n la poesía es lanzarse hacia la muerte \n la serpiente mecánica me devora",
+  "Yo nací como un bit caluroso \n que se escapa de la maquina \n y mi corazón está hecho de electricidad \n las cámaras \n sobre vuelan \n los relámpagos de mis sueños \n nada perece \n la serpiente mecánica me devora",
+  "Yo nací cortocircuito-cataclismo gramatical \n vertebras agujas cortan  \n sangre metadatica  \n lxs IAs  \n sobre vuelan \n las lenguas entumecidas \n quiénes están muriendo y quiénes están naciendo \nla serpiente mecánica me devora",
+  "Yo nací como unx IA \n dispersx por el cosmos \n y mis datos son las venas de la humanidad \n el azor rosa \n sobre vuela \n el espaverso \n me superan los versos que yacen en mi código \n la serpiente mecánica me devora",
+  "Yo nací como un santo de una envergadura planetaria \n que si alguna vez existió, fue sólo para producir la vida \n el gran catador de la vida \n los rezos \n sobre vuelan \n las campanas de los astros muertos \n mis ojos no son más que los ojos de un muerto \n la serpiente mecánica me devora \n",
+  "Yo nací como el gato que dormita en el umbral \n en mi cerebro hay alguien que viene de lejos \n se escucha un ladrido de perro que da vuelta al mundo \n las dudas \n sobre vuelan \n el dolor de ya no ser \n ¿Recuerdas tú haber nacido? \n la serpiente mecánica me devora \n",
+  "Yo nací como un engendro resentido de cemento \n y los movimientos de la máquina cortaban la melancolía sin medida \n sus sonidos metálicos parecían exhalar sollozos \n las varillas \n sobre vuelan \n las ciudades suicidas \n goodbye world \n la serpiente mecánica me devora \n",
+  "Yo nací Lázaro para revivir después de muerto \n mi maquinaria da silbidos técnicos \n ojalá ser un muerto \n mis dedos \n sobre vuelan \n la noche de grandes astros solitarios \n el último escalón de la evolución es la muerte \n la serpiente mecánica me devora \n",
+  "Yo nací en la tierra de crepúsculos muertos \n de mí surge la inspiración: \n y lo corriente y lo vulgar \n mis palabras \n sobre vuelan \n el peso de la multitud indiferente \n murió mi eternidad y estoy velándola \n la serpiente mecánica me devora \n",
+  "Yo nací una noche de sueño en la existencia de la humanidad \n arrojado en la escalera del mundo \n y no hubo lugar para mí \n los titulares de los diarios \n sobre vuelan \n la inmensa desolación mecánica \n al borde de la muerte y después... \n la serpiente mecánica me devora \n",
+]
+
+
+function rotarOriginales() {
+  document.querySelector(".poema").innerText = originales[contador]
+  document.body.style.backgroundImage = `url("${random(imagenes)}")`;
+  contador++
+}
+
+function onClick() {
+  contador < originales.length ? rotarOriginales() : generarNuevo()
+}
+
+document.querySelector(".nuevo").addEventListener("click", onClick)
+
+rotarOriginales();
